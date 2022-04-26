46c483664b8693bbd5d3cdfe0c209c299238f43c
Jonab <jonabsf@gmail.com>
2022-04-26 04:52:27 -0300

    Implementação da API de Aulas

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,16 @@
 
 use App\Http\Controllers\Api\{
     CourseController,
-    ModuleController
+    ModuleController,
+    LessonController
 };
 use Illuminate\Support\Facades\Route;
 
 Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/courses/{id}', [CourseController::class, 'read']);
 Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+Route::get('/lessons/{id}', [LessonController::class, 'read']);
 Route::get('/', function(){
 
     return response()->json([
