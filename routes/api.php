cfd82b91575025c7440045629b416c152e57e4a9
Jonab <jonabsf@gmail.com>
2022-05-01 12:17:49 -0300

    Fix Routes... Again

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Api\{
 use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/auth', [AuthController::class, 'auth']);
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/courses', [CourseController::class, 'index']);
     Route::get('/courses/{id}', [CourseController::class, 'read']);
